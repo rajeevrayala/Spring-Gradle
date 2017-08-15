032a4c0d478ed1452bf64c8bc1b2815813c8f45c
rayala <rayala@192.168.1.33>
2017-08-15 17:42:12 -0500

    2nd commit

--- demo2-gradle/src/main/java/com/javainuse/BootGradleApplication.java
+++ demo2-gradle/src/main/java/com/javainuse/BootGradleApplication.java
@@ -2,6 +2,8 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class BootGradleApplication {
@@ -9,4 +11,5 @@ public class BootGradleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootGradleApplication.class, args);
 	}
+
 }
